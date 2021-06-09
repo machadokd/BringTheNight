1996a6c97f673931d115367810bf3a254b25f8bf
nunoaevedo <NrCa.1999>
2021-06-09 23:09:09 +0100

    Criado perfil do estabelecimento.

---
+++ app/src/main/java/ipvc/estg/bringthenight/CriarEventoActivity.kt
@@ -0,0 +1,11 @@
+package ipvc.estg.bringthenight
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CriarEventoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_criar_evento)
+    }
+}
--- app/src/main/java/ipvc/estg/bringthenight/FeedEmpresasActivity.kt
+++ app/src/main/java/ipvc/estg/bringthenight/FeedEmpresasActivity.kt
@@ -1,11 +1,20 @@
 package ipvc.estg.bringthenight
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_feed_empresas.*
 
 class FeedEmpresasActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feed_empresas)
+
+        new_post.setOnClickListener {
+            val intent = Intent(this@FeedEmpresasActivity, CriarEventoActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
