93cf043145201f8d5c4835a8a5f27a4420cf01de
nunoaevedo <NrCa.1999>
2021-06-17 17:00:38 +0100

    perfil empresa para user

---
+++ app/src/main/java/ipvc/estg/bringthenight/PerfilEmpresaUserActivity.kt
@@ -0,0 +1,17 @@
+package ipvc.estg.bringthenight
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PerfilEmpresaUserActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil_empresa_user)
+    }
+
+
+
+}
